d7f38aa05f8bc3711af52c3b4a9e43851f7203de
Md. Shamiul Islam Shakil <75733493+mdshakil20@users.noreply.github.com>
2022-10-27 10:25:52 +0600

    blogs cards  added

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,9 +1,62 @@
 import React from "react";
+import LeftSideBar from "../Shared/LeftSideBar";
 
 const Blog = () => {
     return (
-        <div>
-            <h3>Blog page</h3>
+        <div className="flex ">
+            <LeftSideBar></LeftSideBar>
+            <div className="grid gap-8 lg:grid-cols-3  md:grid-cols-2  sm:grid-cols-1 m-10">
+                <div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            What is cors?
+                        </h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Cross-origin resource sharing (CORS) is a browser mechanism which enables controlled access to resources located outside of a given domain. It extends and adds flexibility to the same-origin policy (SOP). However, it also provides potential for cross-domain attacks, if a website's CORS policy is poorly configured and implemented. CORS is not a
+                        protection against cross-origin attacks such as cross-site request forgery (CSRF).
+                    </p>
+                </div>
+                <div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Why are you using firebase? What other options do you have to implement authentication?
+                        </h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Firebase helps you develop high-quality apps, grow your user base, and earn more money. Each feature works independently, and they work even better together.
+                        <br />
+                        <br />
+                        Usually, authentication by a server entails the use of a user name and password. Other ways to authenticate can be through cards, retina scans, voice recognition, and fingerprints.
+                    </p>
+                </div>
+                <div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            How does the private route work?
+                        </h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        The react private route component renders child components ( children ) if the user is logged in. If not logged in the user is redirected to the /login page with the return url
+                        passed in the location state property.
+                    </p>
+                </div>
+                <div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            What is Node? How does Node work?
+                        </h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        A node is a basic unit of a data structure, such as a linked list or tree data structure. Nodes contain data and also may link to other nodes. Links between nodes are often implemented by pointers.
+                        <br />
+                        <br />
+                        Node is completely event-driven. Basically the server consists of one thread processing one event after another. A new request coming in is one kind of event. The server starts processing it and when there is a blocking IO operation,
+                        it does not wait until it completes and instead registers a callback function
+                    </p>
+                </div>
+
+            </div>
         </div>
     );
 }
